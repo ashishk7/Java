4a2449fb3d9f21779f03385b664ba5369d005560
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-04 22:25:24 +0530

    commit

--- 6. Functions/b. Volume Of Sphere/src/Main.java
+++ 6. Functions/b. Volume Of Sphere/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
+    public static int solve(int A) {
+        double Volume =  Math.ceil((4*Math.PI * A * A * A) / 3); //volume of sphere is 4/3*pi*r*r*r
+        return  (int)Volume;                                     // convert double to int
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Radius: ");
+            int A = scanner.nextInt();                            //take input from the user
+            System.out.print("Volume is: " + solve(A));
     }
 }
