fec758864d09ac3219dee70b6e4c7d33d6b2c70f
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-12 03:18:56 +0530

    consecutive duplicates

--- 7. 1D Arrays/p. Consecutive Duplicate/src/Main.java
+++ 7. 1D Arrays/p. Consecutive Duplicate/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
+    public static boolean pairCount(int[] A) {
+        for(int i = 0 ; i<A.length-1; i++){
+            if (A[i] == A[i+1])return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Length of the Array: ");
+        int N = scanner.nextInt();  //take input from the user for length of the array
+        int[] A = new int[N];
+
+        System.out.print("Enter Elements of the Array: ");
+        for(int j=0 ; j<N ; j++){   //start a loop to input N elements of the array
+            A[j]=scanner.nextInt();
+        }
+
+        System.out.print(pairCount(A));
+
     }
 }
