a0e3fcbbfb546df8daf5aa6fca1e8579dfeeda27
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-12 21:11:33 +0530

    commit

--- 8. 2D Arrays/f. Sum of Columns of 2D Array/src/Main.java
+++ 8. 2D Arrays/f. Sum of Columns of 2D Array/src/Main.java
@@ -1,25 +1,41 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter rows in Array: ");
-        int N = scanner.nextInt();
-        System.out.print("Enter columns in Array: ");
-        int M = scanner.nextInt();
-        int[][] A = new int[N][M];
+import java.util.Random;
 
-        System.out.print("Enter Elements of the Array: ");
-        for(int row=0 ; row<N ; row++){
-            for(int col=0; col<M; col++){
-                A[row][col]=scanner.nextInt();
+public class Main {
+    public static int[] solve(int A, int B, int[][] C) {
+    int[] columnSumMatrix = new int[C[0].length];
+        for (int col = 0; col<C[0].length; col++) {
+            for (int row = 0; row < C.length; row++) {
+                columnSumMatrix[col] = C[row][col];
             }
         }
+        return columnSumMatrix;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();                   // Creating an instance of the Random class
+        System.out.print("Enter rows in Array: ");
+        int A = scanner.nextInt();
+        System.out.print("Enter columns in Array: ");
+        int B = scanner.nextInt();
 
-        for(int row=0 ; row<N ; row++){
-            for(int col=0; col<M; col++){
-                System.out.print(A[row][col]+" ");
+        int[][] C = new int[A][B];
+        int[] sumOfColumns = new int[B];
+        //Generate and print Matrix
+        System.out.println("Generated Matrices: ");
+        for (int row = 0; row < A; row++) {
+            for (int col = 0; col < B; col++) {
+                C[row][col] = random.nextInt(10, 100);  //Generating Matrix C using random integers between 10(inclusive) and 100 (exclusive)
+                System.out.print(C[row][col] + " ");                 //Print elements of the matrix as generated
             }
             System.out.println();
         }
+
+        sumOfColumns = solve(A,B,C);
+
+        for(int i = 0; i<B; i++){
+            System.out.print(sumOfColumns[i]+" ");
+        }
+
     }
 }
