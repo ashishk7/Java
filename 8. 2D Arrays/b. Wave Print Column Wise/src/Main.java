b63ac8b6296553c54dbe9b3f4fbc167e2337a9fc
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-12 17:49:11 +0530

    wave print

--- 8. 2D Arrays/b. Wave Print Column Wise/src/Main.java
+++ 8. 2D Arrays/b. Wave Print Column Wise/src/Main.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter rows in Array: ");
+        System.out.print("Enter NxN Matrix Length: ");
         int N = scanner.nextInt();
-        System.out.print("Enter columns in Array: ");
-        int M = scanner.nextInt();
-        int[][] A = new int[N][M];
+        int[][] A = new int[N][N];
 
         System.out.print("Enter Elements of the Array: ");
         for(int row=0 ; row<N ; row++){
-            for(int col=0; col<M; col++){
+            for(int col=0; col<N; col++){
                 A[row][col]=scanner.nextInt();
             }
         }
 
         for(int col=0 ; col<N ; col++){
-            for(int row=0; row<M; row++){
-                System.out.print(A[row][col]+" ");
+            if(col%2==0){
+                for(int row=0; row<N; row++){
+                    System.out.print(A[row][col]+" ");
             }
 
-            col++;
-            for(int row=M-1; row>=0; row--){
-                System.out.print(A[row][col]+" ");
+            }
+            if(col%2!=0){
+                for(int row=N-1; row>=0; row--){
+                    System.out.print(A[row][col]+" ");
+            }
+
             }
         }
     }
--- 8. 2D Arrays/c. Wave Print Row Wise/src/Main.java
+++ 8. 2D Arrays/c. Wave Print Row Wise/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter NxN Matrix Length: ");
+        int N = scanner.nextInt();
+        int[][] A = new int[N][N];
+
+        System.out.print("Enter Elements of the Array: ");
+        for(int row=0 ; row<N ; row++){
+            for(int col=0; col<N; col++){
+                A[row][col]=scanner.nextInt();
+            }
+        }
+
+        for(int row=0 ; row<N ; row++){
+            if(row%2==0){
+                for(int col=0; col<N; col++){
+                    System.out.print(A[row][col]+" ");
+                }
+            }
+            if(row%2!=0){
+                for(int col=N-1; col>=0; col--){
+                    System.out.print(A[row][col]+" ");
+                }
+            }
+        }
     }
 }
