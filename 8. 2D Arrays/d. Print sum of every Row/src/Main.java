bd2ca712ff34fccb06191325e9281c61860fd360
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-12 19:02:24 +0530

    commit

--- 8. 2D Arrays/d. Print sum of every Row/src/Main.java
+++ 8. 2D Arrays/d. Print sum of every Row/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter rows in Array: ");
+        int N = scanner.nextInt();
+        System.out.print("Enter columns in Array: ");
+        int M = scanner.nextInt();
+        int[][] A = new int[N][M];
+
+        System.out.print("Enter Elements of the Array: ");
+        for(int row=0 ; row<N ; row++){
+            for(int col=0; col<M; col++){
+                A[row][col]=scanner.nextInt();
+            }
+        }
+
+        for(int row=0 ; row<N ; row++){
+            int sum = 0;
+            for(int col=0; col<M; col++){
+                sum= sum+A[row][col];
+            }
+            System.out.print(sum+" ");
+        }
     }
 }
---
+++ 8. 2D Arrays/f. Sum of Columns of 2D Array/Sum of Columns.md
@@ -0,0 +1,40 @@
+Problem Description
+Given a 2D integer array C[][] of A rows and B columns. Return an integer array of size B that represents the sums of the columns of the 2D array C.
+
+Problem Constraints
+1 <= A,B <= 103
+1 <= C[i][j] <= 103
+
+Input Format
+The first argument is a single integer A.
+The second argument is a single integer B.
+The third argument is a 2D integer array C.
+
+Output Format
+Return an integer array of size B that represents the sums of the columns of the 2D array C.
+
+Example Input
+Input 1:
+A = 3
+B = 2
+C = [[4, 1], [1, 3], [6, 2]]
+Input 2:
+A = 2
+B = 2
+C = [[1, 1], [4, 1]]
+
+Example Output
+Output 1:
+[11, 6]
+Output 2:
+[5, 2]
+
+Example Explanation
+Explanation 1:
+ Column 1 : 4 + 1 + 6 = 11
+Column 2 : 1 + 3 + 2 = 6
+Explanation 2:
+
+Column 1 : 4 + 1 = 5
+Column 2 : 1 + 1 = 2
+
