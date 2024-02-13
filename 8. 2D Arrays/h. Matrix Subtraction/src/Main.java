aec14d597e237607961b8d8d41c91f68e3adcb65
Ashish Kumar <ashishmarch12@gmail.com>
2024-02-13 17:01:19 +0530

    commit

--- 8. 2D Arrays/h. Matrix Subtraction/src/Main.java
+++ 8. 2D Arrays/h. Matrix Subtraction/src/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static int[][] solve(int[][] A, int[][] B) {
-        int[][] newMatrix = new int[A.length][B[0].length]; //not using B[0].length will give error out of bound
+    public static int solve(int[][] A, int[][] B) {
         for(int row = 0 ; row<A.length ; row++){
             for (int col=0; col<B[0].length; col++){
-                newMatrix[row][col]=A[row][col]-B[row][col];
+                if(A[row][col]!=B[row][col]){
+                    return 0;
+                }
             }
         }
-        return newMatrix;
+        return 1;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,13 +22,17 @@ public class Main {
 
         int[][] A = new int[sizeA][sizeB];
         int[][] B = new int[sizeA][sizeB];
-        int[][] newMatrix = new int[A.length][B[0].length];
+        System.out.print("Generate Equal Matrices?(y/n): ");
+        char ans = scanner.next().charAt(0);
 
         System.out.println("Generated Matrices: ");
         for (int row = 0; row < sizeA; row++) {
             for (int col = 0; col < sizeB; col++) {
-                A[row][col] = random.nextInt(10, 100);  //Generating Matrix A using random integers between 10(inclusive) and 100 (exclusive)
-                B[row][col] = random.nextInt(10, 100);  //Generating Matrix A using random integers between 10(inclusive) and 100 (exclusive)
+                A[row][col] = random.nextInt(1, 10);
+                if(ans=='n'){
+                    B[row][col] = random.nextInt(1, 10);
+                }
+                else{B[row][col]=A[row][col];}
             }
         }
 
@@ -45,14 +50,6 @@ public class Main {
             }
             System.out.println();
         }
-        System.out.println();
-        System.out.println("New Matrix after addition of Elements: ");
-        newMatrix = solve(A,B);                                     //Calling solve function to add elements of matrices
-        for (int row = 0; row < sizeA; row++) {                      //Printing New Matrix after addition of the Elements of two matrices
-            for (int col = 0; col < sizeB; col++) {
-                System.out.print(newMatrix[row][col] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("\n"+solve(A,B));
     }
 }
--- 8. 2D Arrays/j. Print matrix column by column/src/Main.java
+++ 8. 2D Arrays/j. Print matrix column by column/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter rows in Array: ");
+        int N = scanner.nextInt();
+        System.out.print("Enter columns in Array: ");
+        int M = scanner.nextInt();
+        int[][] A = new int[N][M];
+
+        System.out.print("Enter Elements of the Array: ");
+        for(int row=0 ; row<N ; row++){
+            for(int col=0; col<M; col++){
+                A[row][col]=scanner.nextInt();
+            }
+        }
+        System.out.print("Generated Matrix: "+"\n");
+        for(int row=0 ; row<N ; row++){
+            for(int col=0; col<M; col++){
+                System.out.print(A[row][col]+" ");
+            }
+            System.out.println();
+        }
+        System.out.print("\n"+"Column by Column Matrix: "+"\n");
+        for(int row=0 ; row<A[0].length ; row++){
+            for(int col=0; col<A.length; col++){
+                System.out.print(A[col][row]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
