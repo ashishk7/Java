a0a56cc73eb1347fd657f2126fa009b0ea1a51c8
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-14 01:13:23 +0530

    comitt

---
+++ 9. Arraylists/a. Divisible by 5 and 7/Divisible by 5 and 7.md
@@ -0,0 +1,30 @@
+# Problem Description:
+
+Write a program that returns the list of elements that are present in the given list and are divisible by 5 and 7.
+
+## Input Format:
+
+```The only argument is the given list of elements```
+
+## Output Format:
+
+```Return a list consisiting of the elements that are divisible by 5 and 7 from the given list.```
+
+## Sample Input:
+
+```java
+[23, 34, 25, 35, 66, 70, 14]
+```
+
+## Sample Output:
+
+```java
+[35, 70]
+```
+
+## Sample explanation:
+
+```java
+Only 35 and 70 are divisible by 5 and 7 from the list [23,34,25,35,66,70,14]
+therefore the list consisting of only these two elements is returned.
+```
--- 9. Arraylists/a. Divisible by 5 and 7/src/Main.java
+++ 9. Arraylists/a. Divisible by 5 and 7/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class Main {
+    public static ArrayList<Integer> solve(ArrayList<Integer> arr) {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i)%5==0 && arr.get(i)%7==0){
+                newList.add(arr.get(i));
+            }
+        }
+        return newList;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>(); // Create an ArrayList object
+        System.out.print("Enter size of ArrayList: ");
+        int size = scanner.nextInt();
+        for(int i = 0; i<size; i++){
+            list.add(random.nextInt(1, 100));
+        }
+        System.out.println("Generated Arraylist: \n"+list+"\n");
+
+        System.out.println("ArrayList of Elements Divisible by 5 and 7: \n"+solve(list));
     }
 }
