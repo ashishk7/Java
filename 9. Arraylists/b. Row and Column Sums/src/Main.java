de4f19002119c6a4d46884b5e1fbd49823c8e680
Ashish Kumar <ashishmarch12@gmail.com>
2024-02-14 15:26:49 +0530

    comitt

--- 9. Arraylists/b. Row and Column Sums/src/Main.java
+++ 9. Arraylists/b. Row and Column Sums/src/Main.java
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-// INCOMPLETE
 import java.util.*;
 public class Main {
 //    public static ArrayList<Integer> solve(ArrayList<Integer> arr) {
@@ -19,20 +13,19 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-
-//        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(); // Create an ArrayList object
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(); // Create an ArrayList object
         System.out.print("Enter rows in ArrayList: ");
         int row = scanner.nextInt();
         System.out.print("Enter columns in ArrayList: ");
         int col = scanner.nextInt();
-        ArrayList<Integer>[][] list = new ArrayList[row][col];
+
         for(int i = 0; i<row; i++){
             for(int j=0; j<col; j++){
-                list[i][j] = new ArrayList<Integer>();
-                list[i][j].add(random.nextInt(1, 100));
+                int r = random.nextInt(1, 100);
+                list.get(0).add(0, 78);
             }
         }
-        System.out.println("Generated Arraylist: \n"+Arrays.deepToString(list)+"\n");
+        System.out.println("Generated Arraylist: \n"+list+"\n");
 
     }
 }
