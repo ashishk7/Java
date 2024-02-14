8b0348149df93b80186e29eb16baaba7f1501ca6
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-14 19:30:50 +0530

    commit

--- 9. Arraylists/b. Row and Column Sums/src/Main.java
+++ 9. Arraylists/b. Row and Column Sums/src/Main.java
@@ -1,15 +1,24 @@
 
 import java.util.*;
 public class Main {
-//    public static ArrayList<Integer> solve(ArrayList<Integer> arr) {
-//        ArrayList<Integer> newList = new ArrayList<Integer>();
-//        for(int i=0;i<arr.size();i++){
-//            if(arr.get(i)%5==0 && arr.get(i)%7==0){
-//                newList.add(arr.get(i));
-//            }
-//        }
-//        return newList;
-//    }
+    public static ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> A) {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        for(int i=0;i<A.size();i++){
+            int rowSum = 0;
+            for(int j=0; j<A.get(i).size(); j++) {
+               rowSum = rowSum+ A.get(i).get(j);
+            }
+           newList.add(rowSum);
+        }
+        for(int i=0;i<A.get(0).size();i++){
+            int colSum = 0;
+            for(int j=0; j<A.size(); j++) {
+                colSum = colSum+ A.get(j).get(i);
+            }
+            newList.add(colSum);
+        }
+        return newList;
+    }
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
@@ -20,12 +29,14 @@ public class Main {
         int col = scanner.nextInt();
 
         for(int i = 0; i<row; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
             for(int j=0; j<col; j++){
-                int r = random.nextInt(1, 100);
-                list.get(0).add(0, 78);
+                int r = random.nextInt(1, 10);
+                temp.add(r);
             }
+            list.add(temp);
         }
         System.out.println("Generated Arraylist: \n"+list+"\n");
-
+        System.out.println("Sum of Rows and Columns: \n"+solve(list));
     }
 }
