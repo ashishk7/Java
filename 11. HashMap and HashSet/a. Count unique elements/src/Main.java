67f8baa91698d6c2625ada8db9fbfb9b9c84a2ab
Ashish Kumar <42992076+ashishk7@users.noreply.github.com>
2024-02-16 23:38:15 +0530

    commit

--- 11. HashMap and HashSet/a. Count unique elements/src/Main.java
+++ 11. HashMap and HashSet/a. Count unique elements/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
 public class Main {
+    public static int solve(ArrayList<Integer> A) {
+        HashSet<Integer> Unique = new HashSet<>();
+        for (int i = 0; i<A.size(); i++){
+            Unique.add(A.get(i));
+        }
+        for(int j=0; j<Unique.size(); j++){
+            freq(A,)
+        }
+        System.out.print(Unique);
+        return 1;
+    }
+    public static int freq(ArrayList<Integer> A)
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter length or Array: ");
+        int size = scanner.nextByte();
+        ArrayList<Integer> A = new ArrayList<>();
+        System.out.print("Enter ArrayList Elements separated by Space: ");
+        for (int i=0; i<size; i++){
+            A.add(scanner.nextInt()) ;
+        }
+        System.out.print(A);
+        System.out.print(solve(A));
+
     }
 }
